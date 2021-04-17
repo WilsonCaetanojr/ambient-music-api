914e4974d9c7689f986065a83a9a6ca5c9c0cf4c
WilsonCaetanjr <wilsonjr_caetano@hotmail.com>
2021-04-17 19:11:27 -0300

    Modifica modelo Albums

--- src/models/Albums.js
+++ src/models/Albums.js
@@ -8,22 +8,34 @@ const attributes = {
     autoIncrement: true,
     allowNull: false,
   },
-  IdUser: {
+  Name: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+    unique: true,
+  },
+  Intensity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+  },
+  IdGenre: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      key: "Id",
+      model: "Genres",
+    },
+  },
+  Description: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+  },
+  createdBy: {
     type: Sequelize.INTEGER,
     allowNull: false,
     references: {
       key: "Id",
       model: "Users",
     },
-  },
-  Name: {
-    type: Sequelize.STRING(50),
-    allowNull: false,
-    unique: true,
-  },
-  Description: {
-    type: Sequelize.STRING(50),
-    allowNull: false,
   },
   createdAt: {
     type: Sequelize.DATE(),
@@ -36,7 +48,7 @@ const attributes = {
   },
 };
 
-const Musics = sequelize.define("Musics", attributes);
+const Albums = sequelize.define("Albums", attributes);
 
-exports.Musics = Musics;
+exports.Albums = Albums;
 exports.attributes = attributes;
